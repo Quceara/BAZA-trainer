fb25c928174c5b51121330c437a2afecb8699b1b
Quceara <nikitka_rozhkov_1337@mail.ru>
2024-06-09 22:38:55 +0500

    правый дравер поменял

--- bazatrainer/lib/InfoWidget.dart
+++ bazatrainer/lib/InfoWidget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class ImageSwiper extends StatefulWidget {
@@ -17,19 +18,64 @@ class _ImageSwiperState extends State<ImageSwiper> {
       'image': 'assets/pictures/Geraklit2.jpg',
       'caption': 'С помощью него вы можете покорять свои личные вершины и развиваться'
     },
+    {
+      'image': 'assets/pictures/krutoy1.jpg',
+      'caption': 'Ничего не бойся, иди вперед и не оборачивайся!'
+    },
+    {
+      'image': 'assets/pictures/krutoy.jpg',
+      'caption': 'Главный враг человека это он сам'
+    },
   ];
 
-  bool isVisible = true;
+  bool isVisible = true; // Инициализация переменной видимости
+  late SharedPreferences _prefs; // Инициализация SharedPreferences
+  late PageController _pageController; // Инициализация контроллера страниц
+  bool isLastSlide = false; // Флаг для определения последнего слайда
+
+  @override
+  void initState() {
+    super.initState();
+    _initPrefs(); // Инициализация SharedPreferences
+    _pageController = PageController(); // Инициализация контроллера страниц
+    _pageController.addListener(_pageListener); // Добавление слушателя для контроллера страниц
+  }
+
+  // Функция для инициализации SharedPreferences
+  Future<void> _initPrefs() async {
+    _prefs = await SharedPreferences.getInstance();
+    // Закомментирована часть кода, отвечающая за проверку кэша и видимость виджета
+    // bool hasAppeared = _prefs.getBool('hasAppeared') ?? false;
+    // setState(() {
+    //   isVisible = !hasAppeared;
+    // });
+  }
+
+  // Функция для установки флага appeared в SharedPreferences
+  Future<void> _setAppeared() async {
+    await _prefs.setBool('hasAppeared', true);
+  }
+
+  // Слушатель для контроллера страниц, определяющий последний слайд
+  void _pageListener() {
+    if (_pageController.page == imagesWithCaptions.length - 1) {
+      setState(() {
+        isLastSlide = true;
+      });
+    } else {
+      setState(() {
+        isLastSlide = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    final PageController _pageController = PageController();
-
     return Visibility(
-      visible: isVisible,
+      visible: isVisible, // Установка видимости виджета
       child: Scaffold(
         body: Container(
-          color: Color.fromARGB(255, 27, 27, 27), // задаем фон
+          color: Color.fromARGB(255, 27, 27, 27), // Задаем фон
           child: Column(
             children: [
               Expanded(
@@ -41,29 +87,33 @@ class _ImageSwiperState extends State<ImageSwiper> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         ClipRRect(
-                          borderRadius: BorderRadius.circular(16.0), // скругление углов
+                          borderRadius: BorderRadius.circular(16.0), // Скругление углов изображения
                           child: SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.6, // 60% от высоты виджета
+                            height: MediaQuery.of(context).size.height *
+                                0.6, // 60% от высоты виджета
                             child: OverflowBox(
-                              maxHeight: MediaQuery.of(context).size.height * 0.6,
+                              maxHeight: MediaQuery.of(context).size.height *
+                                  0.6,
                               child: Image.asset(
                                 imagesWithCaptions[index]['image']!,
-                                fit: BoxFit.cover, // обрезаем изображение
+                                fit: BoxFit.cover, // Обрезаем изображение
                               ),
                             ),
                           ),
                         ),
                         SizedBox(height: 20),
                         Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 16.0), // добавляем горизонтальные отступы
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 16.0), // Добавляем горизонтальные отступы
                           child: Text(
                             imagesWithCaptions[index]['caption']!,
                             style: TextStyle(
                               fontSize: 15,
                               fontWeight: FontWeight.bold,
-                              color: Colors.white, // делаем текст белым для контраста
+                              color: Colors.white, // Делаем текст белым для контраста
                             ),
-                            textAlign: TextAlign.center, // центрируем текст
+                            textAlign:
+                            TextAlign.center, // Центрируем текст
                           ),
                         ),
                       ],
@@ -88,20 +138,22 @@ class _ImageSwiperState extends State<ImageSwiper> {
                 child: Align(
                   alignment: Alignment.centerRight,
                   child: TextButton(
-                    onPressed: () {
+                    onPressed: () async {
+                      await _setAppeared();
                       setState(() {
                         isVisible = false; // Установка видимости в false при нажатии на кнопку
                       });
                     },
                     child: Text(
-                      'Пропустить',
+                      isLastSlide ? 'Я готов!' : 'Пропустить',
                       style: TextStyle(
-                        color: Colors.white, // делаем текст белым для контраста
+                        color: Colors.white, // Делаем текст белым для контраста
                       ),
                     ),
                     style: TextButton.styleFrom(
                       backgroundColor: Colors.transparent,
-                      padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
                     ),
                   ),
                 ),
